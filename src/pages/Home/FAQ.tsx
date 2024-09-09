10630aa19cee8ec7f4f5273029bc96103f451296
imtiazimran <imtiazimran@outlook.com>
2024-09-09 17:43:01 +0600

    faq shaped changed

--- src/pages/Home/FAQ.tsx
+++ src/pages/Home/FAQ.tsx
@@ -128,10 +128,10 @@ export function FAQ() {
               <div className="flex flex-col">
                 <motion.h2
                   layoutId={`question-${faq.question}-${id}`}
-                  className="relative inline-flex  overflow-hidden rounded-full p-0.5  focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+                  className="relative inline-flex  overflow-hidden p-0.5  focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
                 >
                   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-                  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-3 text-sm font-medium text-white backdrop-blur-3xl">
+                  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center bg-slate-950 px-3 py-3 text-sm font-medium text-white backdrop-blur-3xl">
                     {faq.question}
                   </span>
                 </motion.h2>
